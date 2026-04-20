20b37c178af62bcf6657bd821220fb1612fbc773
Mike Kidushim <mike@zorakle.net>
2026-04-20 16:29:06 -0600

    wip

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
+
+import icon from "astro-icon";
 // https://astro.build/config
 
 export default defineConfig({
     site: "https://zorakleprofiles.github.io",
     base: "/wp-update/",
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    integrations: [icon()],
 });

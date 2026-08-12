0660d497c623a438b612cecead8c72765ea31e5a
Mike Kidushim <mike@zorakle.net>
2026-08-12 16:37:53 -0600

    bugfix

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -5,9 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    format: 'file'
-  },
   site: "https://www.zorakleprofiles.com",
   base: "/",
   cacheDir: "../node_modules/.astro",
